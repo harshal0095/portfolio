23f410f7f7599b60c6db332b35dfbb88a39c4539
Harshal Panchal <98376040+harshal0095@users.noreply.github.com>
2024-06-12 18:55:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200});
 
 
 document.getElementById('downloadButton').addEventListener('click', function() {
-    var imageUrl = 'Resume.png'; // Replace with the actual path to your image file
+    var imageUrl = 'assets/img/Resume.png'; // Replace with the actual path to your image file
 
     // Create elements for the image and close button
     var overlay = document.createElement('div');
@@ -95,4 +95,19 @@ document.getElementById('downloadButton').addEventListener('click', function() {
     overlay.appendChild(img);
     overlay.appendChild(closeButton);
     document.body.appendChild(overlay);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const themeToggle = document.getElementById('theme-toggle');
+    const themeIcon = document.getElementById('theme-icon');
+    
+    themeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        
+        if (document.body.classList.contains('dark-mode')) {
+            themeIcon.classList.replace('bx-moon', 'bx-sun');
+        } else {
+            themeIcon.classList.replace('bx-sun', 'bx-moon');
+        }
+    });
 });
