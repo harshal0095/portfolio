ecfa40ad5e2ff3284e7f97a4330c3a5d55b8b2fb
Harshal Panchal <98376040+harshal0095@users.noreply.github.com>
2024-07-12 17:57:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -166,16 +166,5 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-document.getElementById('bg-change-btn').addEventListener('click', function() {
-    document.body.style.backgroundColor = getRandomColor();
-});
 
-function getRandomColor() {
-    let letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
 
